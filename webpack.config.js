dbb00a436c5a038a1721c72afd8b429a54f24bda
Hector Chocobar-Torrejon <45291267+hchocobar@users.noreply.github.com>
2024-07-15 22:26:16 +0000

    chau prettier

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,5 @@
 const path = require('path');
 
-const PrettierPlugin = require("./_utils/prettier.js");
 const cleanStack = require("./_utils/clean-stack.js");
 const FriendlyErrorsWebpackPlugin = require('friendly-errors-webpack-plugin');
 const ErrorOverlayPlugin = require('error-overlay-webpack-plugin');
@@ -73,9 +72,6 @@ module.exports = {
     new HtmlWebpackPlugin({
         filename: "index.html",
         template: "src/index.html"
-    }),
-    new PrettierPlugin({
-        failSilently: true
-    }),
+    })
   ]
 };
